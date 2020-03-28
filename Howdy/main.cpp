4bfaccaf350263a49c8b65bd2610464a6c0babbb
stmscavengerhunt <thinthazin.chris@gmail.com>
2020-03-27 21:36:05 -0400

    First Commit

---
+++ Howdy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Howdy
+//
+//  Created by Thin Thazin on 3/27/20.
+//  Copyright © 2020 Thin Thazin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
